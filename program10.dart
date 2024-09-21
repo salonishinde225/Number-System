1f4644d7f54f677b9f9bbb45f246568288dab0f6
salonishinde225 <salonishinde33@gmail.com>
2024-09-21 14:06:57 +0530

    Update program10.dart

--- program10.dart
+++ program10.dart
@@ -4,7 +4,7 @@ void main(){
   int num = int.parse(stdin.readLineSync()!);
   int a= 0;
   int b= 1;
-  for(int i = 1; i<=num; i++);
+  for(int i = 1; i<=num; i++){
     stdout.write("$a ");
      int c = a+b;
     a =b;
