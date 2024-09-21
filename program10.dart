a85a2727fc5b2ed081c4ad6d49db3826f6164b82
salonishinde225 <salonishinde33@gmail.com>
2024-09-21 14:04:09 +0530

    Update program10.dart

--- program10.dart
+++ program10.dart
@@ -2,8 +2,8 @@ import'dart:io';
 void main(){
   print("num :");
   int num = int.parse(stdin.readLineSync()!);
-  int a= 1;
-  int b= 0;
+  int a= 0;
+  int b= 1;
   for(int i = 1; i<=num; i++);
     stdout.write("$a ");
      int c = a+b;
